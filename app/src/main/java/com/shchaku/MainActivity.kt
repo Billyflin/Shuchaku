5ebb2942b36e4313c42318d9f427ac422208aef6
Billy <Billyflin>
2021-12-16 00:54:36 -0300

    uwu

--- app/src/main/java/com/shchaku/MainActivity.kt
+++ app/src/main/java/com/shchaku/MainActivity.kt
@@ -5,44 +5,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
+import com.shchaku.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        lateinit var binding: ActivityManagerQrBinding
-//
-        setContentView(savedInstanceState)
-        binding = ActivityManagerQrBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnScan.setOnClickListener { initScan() }
-        setContentView(R.layout.activity_main)
+        binding.leer.setOnClickListener { initScan() }
     }
 
-            private fun setContentView(savedInstanceState: Bundle?) {
+    private fun initScan() {
+        val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+        integrator.setPrompt("version inicial")
+        integrator.setBeepEnabled(true)
+        integrator.initiateScan()
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        if (result != null){
+            if (result.contents==null){
+                Toast.makeText(this,"Cancelado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this,"El contenido del qr es: ${result.contents}", Toast.LENGTH_SHORT).show()
             }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 
-            private fun initScan() {
-                val integrator = IntentIntegrator(this)
-                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
-                integrator.setPrompt("version inicial")
-                integrator.setBeepEnabled(true)
-                integrator.initiateScan()
-            }
-
-            override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-                var result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-                if (result != null) {
-                    if (result.contents == null) {
-                        Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(
-                            this,
-                            "El contenido del qr es: ${result.contents}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                } else {
-                    super.onActivityResult(requestCode, resultCode, data)
-                }
-            }
 }
